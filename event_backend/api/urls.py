85854807747a6d3206afc062c5d30b820f3120da
Satyam0204 <satyampattanaik007@gmail.com>
2023-01-06 17:32:18 +0530

    added routes of login and registration

---
+++ event_backend/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('',viewRoutes, name='viewRoutes'),
+    path('register/',Register.as_view(),name='register'),
+    path('login/',  TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    ]
